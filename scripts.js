af177e26f8c6b3393cbde19df62dfec5afc0ce98
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-12 14:31:10 -0230

    Contact JS working, logs results to console

--- scripts.js
+++ scripts.js
@@ -2,23 +2,27 @@
 //Software Development Portfolio
 //August 5/2024 - August 12, 2024
 
-let contactForm = document.getElementById("contactForm");
+document.addEventListener("DOMContentLoaded", () => {
+  let contactForm = document.getElementById("contactForm");
 
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
 
-  let contactName = document.getElementById("inputName");
-  let contactEmail = document.getElementById("inputEmail");
-  let contactMessage = document.getElementById("inputMessage");
+    let contactName = document.getElementById("inputName");
+    let contactEmail = document.getElementById("inputEmail");
+    let contactMessage = document.getElementById("inputMessage");
 
-  if (
-    contactName.value == "" ||
-    contactEmail.value == "" ||
-    contactMessage.value == ""
-  ) {
-    alert("Ensure you input a value in both fields!");
-  } else {
-    // perform operation with form input
-    alert("This form has been successfully submitted!");
-  }
+    if (
+      contactName.value == "" ||
+      contactEmail.value == "" ||
+      contactMessage.value == ""
+    ) {
+      alert("Ensure you input a value in both fields!");
+    } else {
+      console.log("Name:", contactName.value);
+      console.log("Email:", contactEmail.value);
+      console.log("Message:", contactMessage.value);
+      alert("This form has been successfully submitted!");
+    }
+  });
 });

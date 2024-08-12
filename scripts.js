86028590fca57843d71c1c1aa05112805682dc0f
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-08-12 14:22:08 -0230

    Javascript Contact submission code

--- scripts.js
+++ scripts.js
@@ -1,3 +1,24 @@
 //Chelsea Slade (Mayne)
 //Software Development Portfolio
-//August 5/2024 -
+//August 5/2024 - August 12, 2024
+
+let contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let contactName = document.getElementById("inputName");
+  let contactEmail = document.getElementById("inputEmail");
+  let contactMessage = document.getElementById("inputMessage");
+
+  if (
+    contactName.value == "" ||
+    contactEmail.value == "" ||
+    contactMessage.value == ""
+  ) {
+    alert("Ensure you input a value in both fields!");
+  } else {
+    // perform operation with form input
+    alert("This form has been successfully submitted!");
+  }
+});
